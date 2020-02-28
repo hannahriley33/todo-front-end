e71b5fd81e343bd5f727a2ef8121a473f9ced4a4
Hannah Riley <riley.hannahm@gmail.com>
2020-02-27 17:09:13 -0800

    added buttons

--- src/TodoAppLogin.js
+++ src/TodoAppLogin.js
@@ -28,8 +28,17 @@ export default class TodoAppLogin extends Component {
     render() {
         return (
             <div>
-                
-            </div>
+                <input value={ this.state.usernameSignUp} onChange={(e) => this.setState({ usernameSignUp: e.target.value})} />
+                <input value={ this.state.passwordSignUp} onChange={(e) => this.setState({ passwordSignUp: e.target.value})} />
+
+                <button onClick={ this.handleSignUp }>Sign Up!</button>  
+                <br/>
+                <input value={ this.state.usernameSignIn} onChange={(e) => this.setState({ usernameSignIn: e.target.value})} />
+                <input value={ this.state.passwordSignIn} onChange={(e) => this.setState({ passwordSignIn: e.target.value})} />
+
+                <button onClick={this.handleSignIn}>Sign In</button>     
+   
+                </div>
         )
     }
 }
